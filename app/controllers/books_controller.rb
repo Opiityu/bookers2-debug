50dd97df766c226ced6d26de7b879205c08e4796
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2023-08-05 08:36:47 +0000

    [Finish]Ad_Task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,10 +26,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    unless is_matching_login_user
+      redirect_to books_path
+    end
     @book = Book.find(params[:id])
   end
 
   def update
+
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
@@ -49,4 +53,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    book.user_id == current_user.id
+  end
+
+
 end
+
